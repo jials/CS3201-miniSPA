f81a187b1e927a5c94108e3cfc67be8dd7e9deb7
soulwraith <siongleng89@gmail.com>
2015-03-22 15:03:34 +0800

    Revert "Added test case and skeleton code"
    This reverts commit 98ebd255eac62541fecf94e00277c01995406c72.

--- EmptyGeneralTesting/UnitTesting/source/UnitTest.cpp
+++ EmptyGeneralTesting/UnitTesting/source/UnitTest.cpp
@@ -1,7 +1,6 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 #include <iostream>
-#include "SimpleParser.h"
 
 using namespace std;
 
@@ -10,8 +9,7 @@ int main(int argc, char* argv[])
 // Get the top level suite from the registry
 CppUnit::TestSuite *unitSuite = new CppUnit::TestSuite( "All unit test" );
 unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("VarTableTest").makeTest());	
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("TestSimpleParser").makeTest());
-unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("TestSimpleValidator").makeTest());
+unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ParserTest").makeTest());
 unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("QueryPreprocessorTest").makeTest());
 unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ProcTableTest").makeTest());	
 CppUnit::TestFactoryRegistry::getRegistry().addTestToSuite(unitSuite);
@@ -21,7 +19,7 @@ CppUnit::TextUi::TestRunner runner;
 runner.addTest(unitSuite);
 bool wasSucessful = runner.run();
 
-getchar();
+//getchar();
 
 return wasSucessful ? 0 : 1;
 }
--- EmptyGeneralTesting/source/SimpleParser.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-
-class SimpleParser
-{
-public:
-   int parse();
-};
