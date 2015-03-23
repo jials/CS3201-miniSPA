459e785bf33dced9176c15c6f48e41ecbae6f866
Liu Kexin <kexinliu131@gmail.com>
2015-03-23 19:25:15 +0800

    Initial Implementation of QueryTreeRoot

---
+++ EmptyGeneralTesting/SPA/PQLPreProcessor.cpp
@@ -0,0 +1,17 @@
+#include "PQLPreProcessor.h"
+#include "QueryTreeNode.h"
+#include <vector>
+
+class PQLPreProcessor{
+public:
+	PQLPreProcessor(){
+	}
+
+	QueryTreeRoot parse(vector<string> strs,string name){
+		QueryTreeRoot result(name);
+		
+		
+		
+		return result;
+	}
+};
