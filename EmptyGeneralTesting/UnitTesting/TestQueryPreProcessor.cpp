a779fe51e3a7cdb8793a5d7e2896321e01aca012
Liu Kexin <kexinliu131@gmail.com>
2015-03-24 15:59:07 +0800

    add in header files

---
+++ EmptyGeneralTesting/UnitTesting/TestQueryPreProcessor.cpp
@@ -0,0 +1,32 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "PQLPreProcessor.h"
+#include "TestQueryPreProcessor.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+void
+QueryPreProcessorTest::setUp()
+{}
+void
+QueryPreProcessorTest::tearDown()
+{}
+
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( QueryPreProcessorTest );
+// method to test the constructor
+void QueryPreProcessorTest::testConstructor() { 
+}
+
+// method to test the assigning and retrieval of grades
+void QueryPreProcessorTest::testParse() {
+	vector<string> strs;
+	PQLPreProcessor processor;
+	QueryTreeRoot res = processor.parse(strs,"");
+	int num;
+	string str;
+
+	CPPUNIT_ASSERT_EQUAL("1",str);
+
+}
