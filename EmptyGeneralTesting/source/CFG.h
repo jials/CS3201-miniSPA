2a792f32785f2e95bcd56c5e75c72d9de161d665
Wenyan <huwy.vio@gmail.com>
2015-03-24 16:10:39 +0800

    CFG API added

--- EmptyGeneralTesting/source/CFG.h
+++ EmptyGeneralTesting/source/CFG.h
@@ -1,19 +1,24 @@
-#pragma once
-
-#include<stdio.h>
-#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostrem>
 #include <vector>
-
-#include "CFGNode.h"
-
-using namespace std;
-
+#include <string>
+#include "CFGnode.h"
 class CFG{
 public:
+	// called by Parser to create CFG
+	void createCFG(int numOfStatement,std::vector<int> instList);
+	// return boolean whether there is a path goes from A to B
+	bool hasPath(int statNumA,int statNumB);
+	//	get all possible Paths from A to B
+	std::vector<int> getPathFromTo(int statNumA,int statNumB);
+
 
 private:
-	std::vector<CFGNode> statmentNodeList;
-	std::vector<CFGNode> dummyNodeList;
-	CFGNode rootNode;
+	int numOfStatementNode;
+	int ** adjTable;
+	std::vector<CFGnode> statNodeList;
+	CFGnode rootNode;
+	void addStatNode();
 
 };
