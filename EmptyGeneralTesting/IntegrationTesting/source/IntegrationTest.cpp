03a01140bbb4ee65ec12a21b290de9eb105cae0b
soulwraith <siongleng89@gmail.com>
2015-04-12 00:21:39 +0800

    Integration testing

--- EmptyGeneralTesting/IntegrationTesting/source/IntegrationTest.cpp
+++ EmptyGeneralTesting/IntegrationTesting/source/IntegrationTest.cpp
@@ -1,6 +1,11 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 #include <iostream>
+#include "SimpleParser.h"
+#include "PQLPreProcessor.h"
+#include "PQLEvaluator.h"
+#include "QueryTreeRoot.h"
+#include "PQLResultNode.h"
 
 
 using namespace std;
@@ -14,14 +19,43 @@ CppUnit::TextUi::TestRunner runner;
 //parse the SIMPLE soource code into the parser
 //Parse();
 
-//Call of DesignExtractor
-//Extract();
+const char *test2 =
+		"procedure Example{ "
+		  "x = 2;"
+		  "z = 3;"
+		  "i = 5;"
+		  "y = x;"
+		  "z = i;"
+		  "x = 10;"
+		 "}";
+
+SimpleParser parser;
+parser.parse(test2);
+vector<string> results;
+string query = "assign a; Select a such that Follows(2, a)";
+PQLPreProcessor proc;
+PQLEvaluator eva;
+QueryTreeRoot root;
+PQLResultNode* node;
+vector<string> a ;
+vector<string> finalResults;
+string first_token = query.substr(0, query.find("Select"));
+string second_token = query.substr(query.find("Select"));
+a.push_back(first_token);
+a.push_back(second_token);
+root = proc.parse(a,"");
+eva.evaluateResult(&root);
+node = root.getResult();
+finalResults = node->getResult();
+for(int q = 0 ; q< finalResults.size(); q++){
+	results.push_back(finalResults[q]);
+}
 
 runner.addTest(suite);
 bool wasSucessful = runner.run();
 
 
-//getchar();
+getchar();
 
 return wasSucessful ? 0 : 1;
 }
